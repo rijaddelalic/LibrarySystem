ae4b38bd8efa98e218db4ed569217d0471dff515
Rijad Delalic <belatica10@gmail.com>
2026-02-10 19:00:56 +0100

    Dodan middleware

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
 from db.connection import Base, engine
-from routers import users, books, loans, stats   # dodaj stats ovdje
-
+from Routers import users, books, loans, stats   # dodaj stats ovdje
+from fastapi.middleware.cors import CORSMiddleware
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Library System API")
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # Registracija svih routera
 app.include_router(users.router)
 app.include_router(books.router)
