38ca544280418f26ef1ed1d82fcfe272cc2dcf95
Rijad Delalic <belatica10@gmail.com>
2026-02-10 20:27:04 +0100

    Sredjena struktura: backend i frontend folderi

--- Models/Book.py
+++
@@ -1,8 +0,0 @@
-class Book:
-    def __init__(self,title:str,author:str,year:int)->None:
-        self.title = title
-        self.author = author
-        self.year = year
-
-    def __str__(self)->str:
-        return f"{self.title} by {self.author} on {self.year}"
